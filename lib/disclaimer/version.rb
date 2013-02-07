ea35ebd4445b1db5b0cead80cb0a2a280845a6db
Rob Nichols <git@nicholshayes.co.uk>
2013-02-07 14:11:27 +0000

    Bump version and start logging version history

--- lib/disclaimer/version.rb
+++ lib/disclaimer/version.rb
@@ -1,3 +1,15 @@
 module Disclaimer
-  VERSION = "0.0.3"
+  VERSION = "0.0.4"
 end
+
+# History
+# =======
+# 
+# 0.0.4 - Improvement
+# -------------------
+# Improves handling of instance where Disclaimer::Document not found
+# 
+# 0.0.3 - Release prior to recording history
+# ------------------------------------------
+# Working version
+#
