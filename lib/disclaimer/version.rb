5e2eace3cf40fea87ffa969cda3f8b4c6492800d
Rob Nichols <git@nicholshayes.co.uk>
2013-06-07 10:51:22 +0100

    Bump to version 0.0.6

--- lib/disclaimer/version.rb
+++ lib/disclaimer/version.rb
@@ -1,11 +1,17 @@
 module Disclaimer
-  VERSION = "0.0.5"
+  VERSION = "0.0.6"
 end
 
 # History
 # 
+# 0.0.6 - Improves redirect mechanism
+# -----------------------------------
+# Uses host apps root path to determine redirect path. This allows modifications
+# to the route caused by the app being mounted in a sub uri, to be taken account
+# of in the redirect url.
+# 
 # 0.0.5 - Bug fix
-# -----------------------
+# ---------------
 # Defaults to '/' if no previous route found
 # 
 # 0.0.4 - Improvement
